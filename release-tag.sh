c5d864579fb61915c0e9bdef84dea308b067e3d6
Christoph Baier <chrisby@crispy-coding.org>
2025-01-11 11:22:17 +0100

    refactoring

--- release-tag.sh
+++ release-tag.sh
@@ -6,7 +6,7 @@ LAST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "The last tag was: $LAST_TAG"
 read -p "Enter the new tag name: " NEW_TAG
 
-git commit -am "refactoring"
+git commit -am "refactoring" || true
 git push
 git tag $NEW_TAG
 git push origin $NEW_TAG
